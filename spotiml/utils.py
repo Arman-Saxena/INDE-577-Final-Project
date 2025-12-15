5b533b5273c97b881814fc6c4b8d75f368f12f29
Arman-Saxena <ajs30@rice.edu>
2025-12-15 12:14:47 -0600

    Update utils.py

--- spotiml/utils.py
+++ spotiml/utils.py
@@ -3,15 +3,9 @@ import numpy as np
 def accuracy(y_true, y_pred):
     return np.mean(y_true == y_pred)
 
-def mse(y_true, y_pred):
-    return np.mean((y_true - y_pred)**2)
-
-def mae(y_true, y_pred):
-    return np.mean(np.abs(y_true - y_pred))
-
 def confusion_matrix(y_true, y_pred):
-    tp = np.sum((y_true==1) & (y_pred==1))
-    tn = np.sum((y_true==0) & (y_pred==0))
-    fp = np.sum((y_true==0) & (y_pred==1))
-    fn = np.sum((y_true==1) & (y_pred==0))
-    return np.array([[tn, fp], [fn, tp]])
+    classes = np.unique(y_true)
+    matrix = np.zeros((len(classes), len(classes)), dtype=int)
+    for t, p in zip(y_true, y_pred):
+        matrix[t][p] += 1
+    return matrix
