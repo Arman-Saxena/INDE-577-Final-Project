8ea279e549f9e918f22e550d182e2f207f453aec
Arman-Saxena <ajs30@rice.edu>
2025-12-15 14:30:27 -0600

    Update knn.py

--- spotiml/knn.py
+++ spotiml/knn.py
@@ -1,31 +1,22 @@
 import numpy as np
-from collections import Counter
 
-class KNN:
-    """
-    K-Nearest Neighbors Classification (manual).
-    """
+class KNNClassifier:
 
-    def __init__(self, k=5):
+    def __init__(self, k=3):
         self.k = k
 
     def fit(self, X, y):
         self.X_train = X
         self.y_train = y
 
-    def _distance(self, a, b):
-        return np.sqrt(np.sum((a - b)**2))
+    def _distance(self, x1, x2):
+        return np.sqrt(np.sum((x1 - x2) ** 2))
+
+    def predict_one(self, x):
+        distances = np.sqrt(np.sum((self.X_train - x) ** 2, axis=1))
+        idx = np.argsort(distances)[:self.k]
+        nearest_labels = self.y_train[idx]
+        return np.round(nearest_labels.mean()).astype(int)
 
     def predict(self, X):
-        preds = []
-        for point in X:
-            distances = [self._distance(point, x_train) for x_train in self.X_train]
-            
-            # get k nearest neighbors
-            k_idx = np.argsort(distances)[:self.k]
-            k_neighbor_labels = [self.y_train[i] for i in k_idx]
-
-            # majority vote
-            most_common = Counter(k_neighbor_labels).most_common(1)[0][0]
-            preds.append(most_common)
-        return np.array(preds)
+        return np.array([self.predict_one(x) for x in X])
