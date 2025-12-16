373a5255effb601583774345bdd0057d47b428d6
Arman-Saxena <ajs30@rice.edu>
2025-12-15 18:03:06 -0600

    Update linear_regression.py

--- spotiml/linear_regression.py
+++ spotiml/linear_regression.py
@@ -1,35 +1,36 @@
 import numpy as np
 
 class LinearRegressionGD:
-    """
-    Linear Regression implemented with Gradient Descent.
-    """
 
-    def __init__(self, lr=0.01, n_iters=1000):
+    def __init__(self, lr=0.01, n_iters=1000, verbose=False):
         self.lr = lr
         self.n_iters = n_iters
+        self.verbose = verbose
         self.weights = None
-        self.bias = None
+        self.bias = 0
+        self.loss_history = []
 
     def fit(self, X, y):
-        # X: (n_samples, n_features)
         n_samples, n_features = X.shape
 
-        # initialize parameters
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        # gradient descent
-        for _ in range(self.n_iters):
+        for i in range(self.n_iters):
+
             y_pred = np.dot(X, self.weights) + self.bias
 
-            # compute gradients
             dw = (1/n_samples) * np.dot(X.T, (y_pred - y))
             db = (1/n_samples) * np.sum(y_pred - y)
 
-            # update parameters
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
+
+            loss = np.mean((y_pred - y)**2)
+            self.loss_history.append(loss)
+
+            if self.verbose and i % 500 == 0:
+                print(f"Iteration {i}, Loss: {loss}")
 
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
